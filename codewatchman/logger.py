b820dbf47d9eba997e78228460c18ac95c0a40cc
Vishnu Pradeep <5638461+thevishnupradeep@users.noreply.github.com>
2024-10-21 00:18:14 +0400

    Added sep, success & failure

--- codewatchman/logger.py
+++ codewatchman/logger.py
@@ -22,25 +22,29 @@ class CodeWatchman(logging.Logger):
 
     def color_message(self, message, level):
         colors = {
-            logging.DEBUG: '\033[94m',    # Blue
-            logging.INFO: '\033[96m',     # gray
+            logging.DEBUG: '\033[97m',    # Blue
+            logging.INFO: '\033[90m',     # gray
             logging.WARNING: '\033[93m',  # Yellow
             logging.ERROR: '\033[91m',    # Red
             logging.CRITICAL: '\033[95m', # Magenta
+            'SUCCESS': '\033[92m',        # Green
         }
+
         reset = '\033[0m' # Reset color
 
         self.debug
 
         return f"{colors.get(level, reset)}{message}{reset}"
 
+    def sep(self):
+        self.info("-" * 120)
+
     def success(self, message):
-        success_message = f"\u2713 {message}"
+        success_message = self.color_message(f"\u2713 {message}", 'SUCCESS')
         self.info(success_message)
 
     def failure(self, message):
-        failure_message = f"\u2717 {message}"
-        self.error(failure_message)
+        self.error(f"\u2717 {message}")
 
     def _log(self, level, msg, args, exc_info=None, extra=None, stack_info=False, **kwargs):
         if self.isEnabledFor(level):
